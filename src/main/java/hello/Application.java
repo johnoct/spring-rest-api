38f57b379011ee4fc1d18863a33bd5ebe95c4b44
John Octubre <johnoctubre7@gmail.com>
2018-01-28 17:37:08 -0500

    intial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author John Octubre
+ * @date 2018-01-28
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+
+    }
+}
---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,23 @@
+package hello;
+
+/**
+ * @author John Octubre
+ * @date 2018-01-28
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content){
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId(){
+        return this.id;
+    }
+
+    public String getContent(){
+        return this.content;
+    }
+}
